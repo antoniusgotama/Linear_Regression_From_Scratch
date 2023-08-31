1a78fb6ee980c13f7be7c1d134543e92517ce440
antoniusgotama <antoniusgotama1321@gmail.com>
2023-08-31 23:48:26 +0700

    second commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def linearRegression():
+    # importing the data
+    data = pd.read_csv("data.csv")
+    print("hello worlds")
+
+if __name__ == "__main__":
+    linearRegression()
